29df43f2f104c59cf2aea50f9e34c13562421d2e
pradipvaghasiya09 <pradipvaghasiya09@gmail.com>
2016-07-20 11:47:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,8 @@
 		
 		<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 		--><script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>			
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		 
 		
        <!-- <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
 		<link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
